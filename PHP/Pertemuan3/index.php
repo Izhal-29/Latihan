05b95c459828d3e2d5cce0024af0ac7d7695a37c
Izhal-29 <isal.informatika@gmail.com>
2022-03-05 12:14:59 +0800

    perubahan Nama folder

---
+++ PHP/Pertemuan3/index.php
@@ -0,0 +1,12 @@
+<?php
+// kontrol flow (struktur kendali) :  mengatur alur pembacaan program
+
+// *****pengulangaan***** 
+// ketika kita ingin megerjakan sebuah blok program tanpa harus menulis sulang scriptnya:
+// for, while, do... while, foreach
+
+// *****Pengkondisian***** 
+// if.. else, if .. else if.. else, ternary, switch
+
+
+?>

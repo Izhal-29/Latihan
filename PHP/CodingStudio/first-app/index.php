1cce5910c58de066b4ce6481ed94648d4c4629a4
Izhal-29 <isal.informatika@gmail.com>
2022-06-29 08:27:05 +0800

    penambahan file index baru

--- PHP/CodingStudio/first-app/index.php
+++ PHP/CodingStudio/first-app/index.php
@@ -1,4 +1,9 @@
 <?php
-  echo('Hello World <br>');
-  echo ('Hello CodingStudio');
+  echo('<p> Hello World </p>');
+  echo ('<h1> Hello CodingStudio </h1>');
+  echo ('<h1> Welcome </h1>');
+
+  $namaku = 'Rizhal';
+
+  echo ($namaku);
 ?>

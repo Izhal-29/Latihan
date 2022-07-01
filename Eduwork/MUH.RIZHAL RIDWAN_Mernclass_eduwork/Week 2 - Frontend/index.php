a53942643c153a288c6f7c53fad325554baf1a66
Izhal-29 <isal.informatika@gmail.com>
2022-07-01 11:48:58 +0800

    Penambahan tugas week 1 - GIT

---
+++ Eduwork/MUH.RIZHAL RIDWAN_Mernclass_eduwork/Week 2 - Frontend/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tugas GIT</title>
+</head>
+<body>
+  <h1>Hello World</h1>
+  <br>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates explicabo nostrum magnam quam, minus nihil voluptatibus. Quas ipsa exercitationem reprehenderit. Veniam repellat iusto ullam voluptas a ut nihil dolor laborum.</p>
+  
+  <h2>Hello Master</h2>
+  <br>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minima quam dicta, qui, at magnam inventore, consectetur corrupti temporibus nisi perspiciatis aliquam natus recusandae atque saepe doloribus voluptatem esse ex. Amet eligendi, nemo fugit ex repudiandae alias praesentium vitae quaerat harum dicta rerum, iste animi officia aliquam aperiam laudantium aliquid, nulla dolore quod laboriosam quam iure assumenda facere. Nobis eius, distinctio quia quas itaque, quaerat quis reprehenderit adipisci saepe odit nam dolores voluptas. Tenetur, nulla minus? Id nulla doloribus distinctio cumque autem modi, magni, aperiam facere, nobis et quos ratione perspiciatis eaque. Iste soluta praesentium, labore officia corrupti nulla explicabo.</p>
+  <ul>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet.</li>
+  </ul>
+</body>
+</html>
